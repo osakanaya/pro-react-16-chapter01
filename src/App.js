8e3b8f05d8e8cc245fef5be939db59093164307b
sashida <sashida@primagest.co.jp>
2020-08-21 02:06:04 +0900

    p.15

--- src/App.js
+++ src/App.js
@@ -10,13 +10,19 @@ export default class App extends Component {
     }
   }
 
-  render() {
-    return (
-      <div>
-        <h4 className="bg-primary text-white text-center p-2">
-          { this.state.userName}'s To Do List
-        </h4>
-      </div>
-    )
+  changeStateData = () => {
+    this.setState({
+      userName: this.state.userName === "Adam" ? "Bob" : "Adam"
+    });
   }
+
+  render = () => 
+    <div>
+      <h4 className="bg-primary text-white text-center p-2">
+        { this.state.userName}'s To Do List
+      </h4>
+      <button className="btn btn-primary m-2" onClick={ this.changeStateData }>
+        Change
+      </button>
+    </div>
 }
